0cfec15d16ab2bbe3eeec38200cb2adbfdae2c6b
Maniac Звинаревский <maks-bot-5@mail.ru>
2017-12-07 01:56:06 +0300

    Закончил верстку

---
+++ CosmeticaShop.Web/Controllers/CabinetController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmeticaShop.Web.Controllers
+{
+    public class CabinetController : Controller
+    {
+        // GET: Cabinet
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
