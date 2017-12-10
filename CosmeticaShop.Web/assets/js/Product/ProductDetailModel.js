54104da3de088fea97048dc1ddd54663de1d2bc3
Maniac Звинаревский <maks-bot-5@mail.ru>
2017-12-10 21:04:14 +0300

    Детальная страница товара

---
+++ CosmeticaShop.Web/assets/js/Product/ProductDetailModel.js
@@ -0,0 +1,75 @@
+﻿var Product = Product || {};
+
+(function () {
+
+    Product.ProductDetailModel = function (theParams) {
+        theParams = theParams || {};
+        this.Id = ko.observable(theParams.Id || "");
+        this.Name = ko.observable(theParams.Name || "");
+        this.Description = ko.observable(theParams.Description || "");
+        this.Price = ko.observable(theParams.Price || 0);
+        this.Discount = ko.observable(theParams.Discount || 0);
+        this.PhotoUrl = ko.observable(theParams.PhotoUrl || "");
+        this.Photos = ko.observableArray(theParams.Photos || []);
+        this.Quantity = ko.observable(theParams.Quantity || 1);
+        this.DateCreate = ko.observable(theParams.DateCreate || "");
+        this.DateCreateView = ko.observable(theParams.DateCreateView || "");
+        this.Content = ko.observable(theParams.Content || "");
+        return this;
+    };
+
+    /**
+    * Определяем конструктор
+    */
+    Product.ProductDetailModel.prototype.constructor = Product.ProductDetailModel;
+
+    /**
+    * Получить данные модели
+    */
+    Product.ProductDetailModel.prototype.GetData = function () {
+        return {
+            Id: this.Id(),
+            Name: this.Name(),
+            Description: this.Description(),
+            Price: this.Price(),
+            Discount: this.Discount(),
+            PhotoUrl: this.PhotoUrl()
+        }
+    }
+
+    /**
+   * Добавить товар в корзину 
+   */
+    Product.ProductDetailModel.prototype.AddProductCart = function () {
+        var self = this;
+        $.post(urlAddProductCart, {
+            productId: this.Id(),
+            quantity: this.Quantity()
+        }).success(function (res) {
+            if (res.IsSuccess) {
+                location.reload();
+            }
+            else {
+                self.ErrorMessage(res.Message);
+            }
+        });
+    }
+    /**
+    * Добавить товар в желаемое 
+    */
+    Product.ProductDetailModel.prototype.AddProductWish = function () {
+        var self = this;
+        $.post(urlAddProductWish, {
+            productId: this.Id()
+        }).success(function (res) {
+            if (res.IsSuccess) {
+                location.reload();
+            }
+            else {
+                self.ErrorMessage(res.Message);
+            }
+        });
+    }
+
+})();
+
---
+++ CosmeticaShop.Web/assets/js/Product/ProductDetailViewModel.js
@@ -0,0 +1,26 @@
+﻿var Product = Product || {};
+
+(function () {
+
+    Product.ProductDetailViewModel = function (theParams) {
+        theParams = theParams || {};
+        this.Model = ko.observable(new Product.ProductDetailModel(theParams.Model));
+        this.SelectQuantity = ko.observableArray(this.GetSelectQuantity());
+        return this;
+    };
+
+    /**
+    * Определяем конструктор
+    */
+    Product.ProductDetailViewModel.prototype.constructor = Product.ProductDetailViewModel;
+
+    Product.ProductDetailViewModel.prototype.GetSelectQuantity = function() {
+        var arr = [];
+        for (var i = 1; i <= 10; i++) {
+            arr.push(i);
+        }
+        return arr;
+    }
+
+
+})();
