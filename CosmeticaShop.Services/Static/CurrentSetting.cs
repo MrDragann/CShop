f30789197999be4fd4f8e78046ef3cb562aa00c0
MrDragan <mrdragann@gmail.com>
2018-04-20 22:40:28 +0300

    правки для моб версии

---
+++ CosmeticaShop.Services/Static/CurrentSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmeticaShop.Services.Static
+{
+    public static class CurrentSetting
+    {
+        public static string PhoneNumber => "074 611 4001";
+
+        public static string DeliveryPrice => "17,10";
+    }
+}
